c57dcc68b6506926d27e9ffef2b763df2952632b
Lucas Duarte <lucasduartedev@gmail.com>
2026-04-27 17:01:21 -0300

    Início do projeto

---
+++ src/main/java/com/api/produtos/ApiProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.api.produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProdutosApplication.class, args);
+	}
+
+}
