f95eeacd831c5a8c6e7b7f7769f40c91efc27f33
Lucas Duarte <lucasduartedev@gmail.com>
2026-04-30 16:26:35 -0300

    Migration da base de dados

---
+++ src/main/resources/db/migration/V1__create-table-produtos.sql
@@ -0,0 +1,8 @@
+CREATE TABLE produtos(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(75) NOT NULL,
+    descricao VARCHAR(150) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    ativo TINYINT NOT NULL,
+    PRIMARY KEY(id)
+);
